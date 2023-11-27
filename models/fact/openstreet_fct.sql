4341bb458c61e4341f8b8043219a06184efdec15
jerome.galam@baruchmail.cuny.edu <jerome.galam@baruchmail.cuny.edu>
2023-11-27 04:59:01 +0000

    1

--- models/fact/openstreet_fct.sql
+++ models/fact/openstreet_fct.sql
@@ -1,19 +1,20 @@
 with all_openstreetrequests as(
     select * from{{ ref('openstreetData_dim')}}
 ),
-location as (
-    select = from{{ ref('openstreetLocation_dim') }}
-),
 request as (
     select = from{{ ref('openstreetRequest_dim') }}
+),
+location as (
+    select = from{{ ref('openstreetLocation_dim') }}
 ),
 dates as (
     select = from{{ ref('openstreetDate_dim') }}
 ),
 join_tbl as(
-    select  openstreetLocation.location_dim_id,
-            openstreetRequest.request_dim_id,
+    select  openstreetRequest.request_dim_id,
+            openstreetLocation.location_dim_id,
             dates.date_dim_id as date_dim_id,
     from all_openstreetrequests
-
+    left join request on all_openstreetrequests.request = request.request
+    
 )
